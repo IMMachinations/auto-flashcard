074b1e01a91ab5fb28e69e1ac18c6911d11093d3
Isaiah <32691164+IMMachinations@users.noreply.github.com>
2024-12-17 17:07:22 -0600

    Update arxiv_calls.py

--- arxiv_calls.py
+++ arxiv_calls.py
@@ -5,5 +5,3 @@ def getArxivPDF(paper_id):
     paper = next(arxiv.Client().results(arxiv.Search(id_list = [paper_id])))
     written_path = paper.download_pdf()
     return written_path
-    
-paper_id = "2410.08201"
